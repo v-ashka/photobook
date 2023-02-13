8e2d6297ef883ed83e471f1e160991cae940f606
v-ashka <mwijaszka99@gmail.com>
2023-02-13 21:52:41 +0100

    created vue project

---
+++ frontend/photo-book/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+// import { RouterLink, RouterView } from 'vue-router'
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'My first Vue app!'
+    }
+  }
+}
+
+</script>
+
+<template>
+ <h1>{{ title }}</h1>
+</template>
